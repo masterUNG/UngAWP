e1f02be53f97bd497878d2f1374f0b55b77696cf
masterUNG <phrombutr@gmail.com>
2020-09-21 14:34:34 +0700

    RedEye

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -6,14 +7,76 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(width: 250,
-          child: Image.asset('images/logo.png'),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              buildContainer(),
+              buildText(),
+              buildContainerUser(),
+              buildContainerPassword(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Container buildContainerUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'User :',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Container buildContainerPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        obscureText: statusRedEye,
+        decoration: InputDecoration(
+          labelText: 'Password :',
+          border: OutlineInputBorder(),
+          suffixIcon: IconButton(
+              icon: Icon(Icons.remove_red_eye),
+              onPressed: () {
+                setState(() {
+                  statusRedEye = !statusRedEye;
+                });
+              }),
+        ),
+      ),
+    );
+  }
+
+  Text buildText() => Text(
+        'Ung PWA',
+        style: GoogleFonts.lobster(
+            textStyle: TextStyle(
+          fontSize: 30,
+          color: Colors.purple.shade700,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold,
+        )),
+      );
+
+  Container buildContainer() {
+    return Container(
+      width: 180,
+      child: Image.asset('images/logo.png'),
     );
   }
 }
