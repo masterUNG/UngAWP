83501160a45b41c4a43f9744096c8a40b9094fca
masterUNG <phrombutr@gmail.com>
2020-09-22 11:08:20 +0700

    test

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -6,6 +6,26 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  List<String> positions = [
+    'Account Manager',
+    'Administrative Officer',
+    'Brand Manager',
+    'Customer Service Executive',
+    'Financial Analyst',
+    'HR Generalist/ Specialist',
+    'Logistic Manager'
+  ];
+  String choosePosition;
+  double lat, lng;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,21 +33,109 @@ class _RegisterState extends State<Register> {
         backgroundColor: Colors.purple.shade700,
         title: Text('Register'),
       ),
-      body: Column(
-        children: [
-          buildAvatar(),
-          buildName(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            buildAvatar(),
+            buildName(),
+            buildSizedBox(),
+            buildPosition(),
+            buildSizedBox(),
+            buildUser(),
+            buildSizedBox(),
+            buildPassword(),
+            buildSizedBox(),
+            buildMap(),
+            buildSizedBox(),
+          ],
+        ),
       ),
     );
   }
+
+  Container buildMap() => Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.width * 0.8,
+        color: Colors.grey,
+      );
+
+  SizedBox buildSizedBox() {
+    return SizedBox(
+      height: 16,
+    );
+  }
+
+  Container buildPosition() => Container(
+        width: 250,
+        child: DropdownButton<String>(
+          items: positions
+              .map(
+                (e) => DropdownMenuItem(
+                  child: Text(e),
+                  value: e,
+                ),
+              )
+              .toList(),
+          value: choosePosition,
+          hint: Text('Positon'),
+          onChanged: (value) {
+            setState(() {
+              choosePosition = value;
+            });
+          },
+        ),
+      );
 
   Container buildName() {
     return Container(
       width: 250,
       child: TextField(
         decoration: InputDecoration(
-            prefixIcon: Icon(Icons.face, color: Color(0xFFE81EE8),),
+            hintText: 'Display Name',
+            prefixIcon: Icon(
+              Icons.face,
+              color: Color(0xFFE81EE8),
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(color: Colors.black38)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(color: Colors.blue))),
+      ),
+    );
+  }
+
+  Container buildUser() {
+    return Container(
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'User',
+            prefixIcon: Icon(
+              Icons.account_circle,
+              color: Color(0xFFE81EE8),
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(color: Colors.black38)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(color: Colors.blue))),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Password',
+            prefixIcon: Icon(
+              Icons.lock,
+              color: Color(0xFFE81EE8),
+            ),
             enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(30),
                 borderSide: BorderSide(color: Colors.black38)),
