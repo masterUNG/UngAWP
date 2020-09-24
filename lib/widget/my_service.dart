16666ee5e51cc4a68fa22d6c55635d3c87092f9d
masterUNG <phrombutr@gmail.com>
2020-09-24 09:37:29 +0700

    update

--- lib/widget/my_service.dart
+++ lib/widget/my_service.dart
@@ -12,37 +12,54 @@ class _MyServiceState extends State<MyService> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              decoration: BoxDecoration(color: Colors.red.shade700),
-              child: ListTile(
-                onTap: () async {
-                  await FirebaseAuth.instance.signOut().then(
-                        (value) => Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Authen(),
-                            ),
-                            (route) => false),
-                      );
-                },
-                leading: Icon(
-                  Icons.exit_to_app,
-                  size: 36,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  'Sign Out',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ],
-        ),
+      drawer: buildDrawer(),
+    );
+  }
+
+  Drawer buildDrawer() {
+    return Drawer(
+      child: Stack(
+        children: [
+          UserAccountsDrawerHeader(accountName: Text('Name'), accountEmail: Text('Email')),
+          buildSingOut(),
+        ],
       ),
     );
+  }
+
+  Column buildSingOut() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.red.shade700),
+          child: ListTile(
+            onTap: () {
+              processSingOut();
+            },
+            leading: Icon(
+              Icons.exit_to_app,
+              size: 36,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Sign Out',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<Null> processSingOut() async {
+    await FirebaseAuth.instance.signOut().then(
+          (value) => Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Authen(),
+              ),
+              (route) => false),
+        );
   }
 }
