fb3c3b867948f2e84d00b87b93285c69374ea4ed
masterUNG <phrombutr@gmail.com>
2020-09-24 14:12:06 +0700

    update

---
+++ lib/utility/constant.dart
@@ -0,0 +1,5 @@
+class Constant {
+  String urlAddFriend = 'https://e7b34503db1b.ngrok.io/pwa/addFriend.php?isAdd=true&uidLogin=$uidLogin&uidFriend=$uidFriend&nameFriend=$nameFriend&position=$position&urlPath=$urlPath&lat=$lat&lng=$lng';
+
+  Constant();
+}
--- lib/widget/read_qr.dart
+++ lib/widget/read_qr.dart
@@ -1,5 +1,8 @@
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ungpwa/models/user_model.dart';
 
 class ReadQR extends StatefulWidget {
   @override
@@ -8,6 +11,8 @@ class ReadQR extends StatefulWidget {
 
 class _ReadQRState extends State<ReadQR> {
   String qrResult;
+  // String qrResult = 'beLSVXcLkMeBeesWgmhk1UIVcNo2';
+  UserModel userModel;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +21,24 @@ class _ReadQRState extends State<ReadQR> {
         onPressed: () => processReadQRcode(),
         child: Icon(Icons.add_to_queue),
       ),
-      body: Center(child: Text('Read QR')),
+      body: qrResult == null
+          ? Center(child: Text('กรุณา คลิก QR code'))
+          : buildColumn(),
+    );
+  }
+
+  Center buildColumn() {
+    return Center(
+      child: Column(
+        children: [
+          Text('uidFrined ==>> $qrResult'),
+          Container(width: 200,height: 200,child: Image.network(userModel.path),),
+          Text('Name ==> ${userModel.name}'),
+          Text('Position ==>> ${userModel.position}'),
+          Text('ละติจูต ==> ${userModel.lat}'),
+          Text('ลองติจูต ==>> ${userModel.lng}'),
+        ],
+      ),
     );
   }
 
@@ -25,6 +47,18 @@ class _ReadQRState extends State<ReadQR> {
       var result = await BarcodeScanner.scan();
       qrResult = result.rawContent;
       print('qrResult = $qrResult');
+
+      await Firebase.initializeApp().then((value) async {
+        await FirebaseFirestore.instance
+            .collection('UserUng')
+            .doc(qrResult)
+            .snapshots()
+            .listen((event) {
+          setState(() {
+            userModel = UserModel.fromJson(event.data());
+          });
+        });
+      });
     } catch (e) {}
   }
 }
