8b7fd692d80133433cd5512080326a06f4590b83
masterUNG <phrombutr@gmail.com>
2020-09-24 15:59:44 +0700

    finish 24 SEc

--- lib/utility/constant.dart
+++ lib/utility/constant.dart
@@ -1,5 +1,5 @@
 class Constant {
-  String urlAddFriend = 'https://e7b34503db1b.ngrok.io/pwa/addFriend.php?isAdd=true&uidLogin=$uidLogin&uidFriend=$uidFriend&nameFriend=$nameFriend&position=$position&urlPath=$urlPath&lat=$lat&lng=$lng';
+  String domain = 'https://e7b34503db1b.ngrok.io';
 
   Constant();
 }
--- lib/widget/read_qr.dart
+++ lib/widget/read_qr.dart
@@ -1,8 +1,11 @@
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ungpwa/models/user_model.dart';
+import 'package:ungpwa/utility/constant.dart';
+import 'package:ungpwa/utility/normal_dialog.dart';
 
 class ReadQR extends StatefulWidget {
   @override
@@ -10,9 +13,16 @@ class ReadQR extends StatefulWidget {
 }
 
 class _ReadQRState extends State<ReadQR> {
-  String qrResult;
-  // String qrResult = 'beLSVXcLkMeBeesWgmhk1UIVcNo2';
+  // String qrResult;
+  String qrResult = 'beLSVXcLkMeBeesWgmhk1UIVcNo2';
   UserModel userModel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +31,7 @@ class _ReadQRState extends State<ReadQR> {
         onPressed: () => processReadQRcode(),
         child: Icon(Icons.add_to_queue),
       ),
-      body: qrResult == null
+      body: userModel == null
           ? Center(child: Text('กรุณา คลิก QR code'))
           : buildColumn(),
     );
@@ -32,15 +42,25 @@ class _ReadQRState extends State<ReadQR> {
       child: Column(
         children: [
           Text('uidFrined ==>> $qrResult'),
-          Container(width: 200,height: 200,child: Image.network(userModel.path),),
+          Container(
+            width: 200,
+            height: 200,
+            child: Image.network(userModel.path),
+          ),
           Text('Name ==> ${userModel.name}'),
           Text('Position ==>> ${userModel.position}'),
           Text('ละติจูต ==> ${userModel.lat}'),
           Text('ลองติจูต ==>> ${userModel.lng}'),
+          buildOutlineButton()
         ],
       ),
     );
   }
+
+  OutlineButton buildOutlineButton() => OutlineButton(
+        onPressed: () => processUploadMySQL(),
+        child: Text('Upload Data To mySQL'),
+      );
 
   Future<Null> processReadQRcode() async {
     try {
@@ -48,17 +68,30 @@ class _ReadQRState extends State<ReadQR> {
       qrResult = result.rawContent;
       print('qrResult = $qrResult');
 
-      await Firebase.initializeApp().then((value) async {
-        await FirebaseFirestore.instance
-            .collection('UserUng')
-            .doc(qrResult)
-            .snapshots()
-            .listen((event) {
-          setState(() {
-            userModel = UserModel.fromJson(event.data());
-          });
+      await readData();
+    } catch (e) {}
+  }
+
+  Future<Null> readData() async {
+    await Firebase.initializeApp().then((value) async {
+      await FirebaseFirestore.instance
+          .collection('UserUng')
+          .doc(qrResult)
+          .snapshots()
+          .listen((event) {
+        setState(() {
+          userModel = UserModel.fromJson(event.data());
+          print('url ==>> ${userModel.path}');
         });
       });
-    } catch (e) {}
+    });
+  }
+
+  Future<Null> processUploadMySQL() async {
+    String urlAPI =
+        '${Constant().domain}/pwa/addFriend.php?isAdd=true&uidLogin=$qrResult&uidFriend=$qrResult&nameFriend=${userModel.name}&position=${userModel.position}&urlPath=${userModel.path}&lat=${userModel.lat}&lng=${userModel.lng}';
+    await Dio()
+        .get(urlAPI)
+        .then((value) => normalDialog(context, 'Upload MySQL Success'));
   }
 }
